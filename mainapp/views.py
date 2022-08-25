ebce7e8187c4baf7130a281650fb3ccd58d11413
Yevgeniy <yevgeniy.karabekov@gmail.com>
2022-08-25 17:25:30 +0600

    lesson_2 Hello world with CBV and FBV

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
 from django.views.generic import View
 
@@ -10,31 +9,3 @@ class HelloWorldView(View):
 
 def check_kwargs(request, **kwargs):
     return HttpResponse(f"kwargs:<br>{kwargs}")
- 
-=======
-from django.views.generic import TemplateView
-
-
-class MainPageView(TemplateView):
-    template_name = "mainapp/index.html"
-
-
-class NewsPageView(TemplateView):
-    template_name = "mainapp/news.html"
-
-
-class CoursesPageView(TemplateView):
-    template_name = "mainapp/courses_list.html"
-
-
-class ContactsPageView(TemplateView):
-    template_name = "mainapp/contacts.html"
-
-
-class DocSitePageView(TemplateView):
-    template_name = "mainapp/doc_site.html"
-
-
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
->>>>>>> lesson_2
